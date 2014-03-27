a5f229be05ce096c07fc4c334ca2aba7f48c0a50
unknown <ernestas.poskus@gmail.com>
2014-03-27 22:20:11 +0200

    Ruby bits 2 level-6 challenges completed

---
+++ ruby bits 2/challenges/dsl/dsl_part_1.rb
@@ -0,0 +1,74 @@
+class Game
+  attr_reader :name
+ 
+  def initialize(name)
+    @name = name
+    @year = nil
+    @system = nil
+  end
+ 
+  def year(value)
+    @year = value
+  end
+ 
+  def system(value)
+    @system = value
+  end
+ 
+  def print_details
+    puts "#{@name} - #{@year} (#{@system})"
+  end
+ 
+  def play
+  end
+ 
+  def capture_screenshot
+  end
+end
+
+class Library
+  def initialize
+    @games = []
+  end
+ 
+  def add_game(game)
+    @games << game
+  end
+ 
+  def find_by_name(name)
+    @games.detect { |game| game.name == name }
+  end
+end
+
+# DSL
+LIBRARY = Library.new
+ 
+def add_game(name, &block)
+  game = Game.new(name)
+  game.instance_eval(&block)
+  LIBRARY.add_game(game)
+end
+ 
+def with_game(name, &block)
+  game = LIBRARY.find_by_name(name)
+  game.instance_eval(&block)
+end
+
+
+
+###########################
+add_game "Civilization" do
+  system "PC"
+  year 1991
+end
+
+add_game "Contra" do
+  system "NES"
+  year 1987
+end
+
+with_game "Contra" do
+  self.capture_screenshot
+  self.print_details
+  self.play
+end
